54a158f0315f5aa32b17d14174e9249038c845f5
MarnuLombard <marnu@lombard>
2014-01-30 21:13:25 +0200

    Changed to after(errorEl) for the sake of screenreaders Screenreaders don't automatically see DOM elements inserted before inputs Inserting errors after the relevant input allows blind people to easily recieve these errors if the site uses happy.js Where I learned this was: http://alistapart.com/article/designing-for-easy-interaction

--- happy.js
+++ happy.js
@@ -4,7 +4,7 @@
   }
   $.fn.isHappy = function (config) {
     var fields = [], item;
-    
+
     function getError(error) {
       return $('<span id="'+error.id+'" class="unhappyMessage">'+error.message+'</span>');
     }
@@ -33,18 +33,18 @@
           id: selector.slice(1) + '_unhappy'
         },
         errorEl = $(error.id).length > 0 ? $(error.id) : getError(error);
-        
+
       fields.push(field);
       field.testValid = function (submit) {
         var val,
           el = $(this),
           gotFunc,
           error = false,
-          temp, 
+          temp,
           required = !!el.get(0).attributes.getNamedItem('required') || opts.required,
           password = (field.attr('type') === 'password'),
           arg = isFunction(opts.arg) ? opts.arg() : opts.arg;
-        
+
         // clean it or trim it
         if (isFunction(opts.clean)) {
           val = opts.clean(el.val());
@@ -53,22 +53,22 @@
         } else {
           val = el.val();
         }
-        
+
         // write it back to the field
         el.val(val);
-        
+
         // get the value
         gotFunc = ((val.length > 0 || required === 'sometimes') && isFunction(opts.test));
-        
+
         // check if we've got an error on our hands
         if (submit === true && required === true && val.length === 0) {
           error = true;
         } else if (gotFunc) {
           error = !opts.test(val, arg);
         }
-        
+
         if (error) {
-          el.addClass('unhappy').before(errorEl);
+          el.addClass('unhappy').after(errorEl);
           return false;
         } else {
           temp = errorEl.get(0);
@@ -82,11 +82,11 @@
       };
       field.bind(config.when || 'blur', field.testValid);
     }
-    
+
     for (item in config.fields) {
       processField(config.fields[item], item);
     }
-    
+
     if (config.submitButton) {
       $(config.submitButton).click(handleSubmit);
     } else {
