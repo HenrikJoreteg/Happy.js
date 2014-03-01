7672b7a7fb3bbbe43fb4fab3b78507e580944d89
Michael Garvin <gar@andyet.net>
2014-02-28 17:08:01 -0800

    Linting and lint hinting

--- happy.js
+++ happy.js
@@ -1,97 +1,98 @@
-(function($){
-  function trim(el) {
-    return (''.trim) ? el.val().trim() : $.trim(el.val());
-  }
-  $.fn.isHappy = function (config) {
-    var fields = [], item;
-
-    function getError(error) {
-      return $('<span id="'+error.id+'" class="unhappyMessage" role="alert">'+error.message+'</span>');
+/*global $*/
+(function ($) {
+    function trim(el) {
+        return (''.trim) ? el.val().trim() : $.trim(el.val());
     }
-    function handleSubmit() {
-      var errors = false, i, l;
-      for (i = 0, l = fields.length; i < l; i += 1) {
-        if (!fields[i].testValid(true)) {
-          errors = true;
+    $.fn.isHappy = function (config) {
+        var fields = [], item;
+
+        function getError(error) {
+            return $('<span id="' + error.id + '" class="unhappyMessage" role="alert">' + error.message + '</span>');
         }
-      }
-      if (errors) {
-        if (isFunction(config.unHappy)) config.unHappy();
-        return false;
-      } else if (config.testMode) {
-        if (window.console) console.warn('would have submitted');
-        return false;
-      }
-    }
-    function isFunction (obj) {
-      return !!(obj && obj.constructor && obj.call && obj.apply);
-    }
-    function processField(opts, selector) {
-      var field = $(selector),
-        error = {
-          message: opts.message || '',
-          id: selector.slice(1) + '_unhappy'
-        },
-        errorEl = $(error.id).length > 0 ? $(error.id) : getError(error);
+        function handleSubmit() {
+            var errors = false, i, l;
+            for (i = 0, l = fields.length; i < l; i += 1) {
+                if (!fields[i].testValid(true)) {
+                    errors = true;
+                }
+            }
+            if (errors) {
+                if (isFunction(config.unHappy)) config.unHappy();
+                return false;
+            } else if (config.testMode) {
+                if (window.console) console.warn('would have submitted');
+                return false;
+            }
+        }
+        function isFunction(obj) {
+            return !!(obj && obj.constructor && obj.call && obj.apply);
+        }
+        function processField(opts, selector) {
+            var field = $(selector),
+            error = {
+                message: opts.message || '',
+                id: selector.slice(1) + '_unhappy'
+            },
+            errorEl = $(error.id).length > 0 ? $(error.id) : getError(error);
 
-      fields.push(field);
-      field.testValid = function (submit) {
-        var val,
-          el = $(this),
-          gotFunc,
-          error = false,
-          temp,
-          required = !!el.get(0).attributes.getNamedItem('required') || opts.required,
-          password = (field.attr('type') === 'password'),
-          arg = isFunction(opts.arg) ? opts.arg() : opts.arg;
+            fields.push(field);
+            field.testValid = function (submit) {
+                var val,
+                el = $(this),
+                gotFunc,
+                error = false,
+                temp,
+                required = !!el.get(0).attributes.getNamedItem('required') || opts.required,
+                password = (field.attr('type') === 'password'),
+                arg = isFunction(opts.arg) ? opts.arg() : opts.arg;
 
-        // clean it or trim it
-        if (isFunction(opts.clean)) {
-          val = opts.clean(el.val());
-        } else if (!opts.trim && !password) {
-          val = trim(el);
+                // clean it or trim it
+                if (isFunction(opts.clean)) {
+                    val = opts.clean(el.val());
+                } else if (!opts.trim && !password) {
+                    val = trim(el);
+                } else {
+                    val = el.val();
+                }
+
+                // write it back to the field
+                el.val(val);
+
+                // get the value
+                gotFunc = ((val.length > 0 || required === 'sometimes') && isFunction(opts.test));
+
+                // check if we've got an error on our hands
+                if (submit === true && required === true && val.length === 0) {
+                    error = true;
+                } else if (gotFunc) {
+                    error = !opts.test(val, arg);
+                }
+
+                if (error) {
+                    el.addClass('unhappy').after(errorEl);
+                    return false;
+                } else {
+                    temp = errorEl.get(0);
+                    // this is for zepto
+                    if (temp.parentNode) {
+                        temp.parentNode.removeChild(temp);
+                    }
+                    el.removeClass('unhappy');
+                    return true;
+                }
+            };
+            field.bind(config.when || 'blur', field.testValid);
+        }
+
+        for (item in config.fields) {
+            processField(config.fields[item], item);
+        }
+
+        if (config.submitButton) {
+            $(config.submitButton).click(handleSubmit);
         } else {
-          val = el.val();
+            this.bind('submit', handleSubmit);
         }
-
-        // write it back to the field
-        el.val(val);
-
-        // get the value
-        gotFunc = ((val.length > 0 || required === 'sometimes') && isFunction(opts.test));
-
-        // check if we've got an error on our hands
-        if (submit === true && required === true && val.length === 0) {
-          error = true;
-        } else if (gotFunc) {
-          error = !opts.test(val, arg);
-        }
-
-        if (error) {
-          el.addClass('unhappy').after(errorEl);
-          return false;
-        } else {
-          temp = errorEl.get(0);
-          // this is for zepto
-          if (temp.parentNode) {
-            temp.parentNode.removeChild(temp);
-          }
-          el.removeClass('unhappy');
-          return true;
-        }
-      };
-      field.bind(config.when || 'blur', field.testValid);
-    }
-
-    for (item in config.fields) {
-      processField(config.fields[item], item);
-    }
-
-    if (config.submitButton) {
-      $(config.submitButton).click(handleSubmit);
-    } else {
-      this.bind('submit', handleSubmit);
-    }
-    return this;
-  };
+        return this;
+    };
 })(this.jQuery || this.Zepto);
--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,4 @@
+/*global $, test, equal, happy, ok, equals*/
 module("Tiny Validator");
 
 function fixture(html) {
@@ -17,84 +18,84 @@ function fill() {
 }
 
 test("check basic required", function () {
-	var form = fixture('<input type="text" id="textInput1" />');
-	
-	form.isHappy({
-	    fields: {
-	        '#textInput1': {
-	            required: true,
-	            message: 'Please enter an email'
-	        }
-	    },
-	    testMode: true
-	});
-	
-	form.trigger('submit');
-	equal($('.unhappy').length, 1);
-	equal($('.unhappyMessage').length, 1);
-	
-	$('#textInput1').val('test');
-	form.trigger('submit');
-	equal($('.unhappy').length, 0);
-	equal($('.unhappyMessage').length, 0);
+    var form = fixture('<input type="text" id="textInput1" />');
+
+    form.isHappy({
+        fields: {
+            '#textInput1': {
+                required: true,
+                message: 'Please enter an email'
+            }
+        },
+        testMode: true
+    });
+
+    form.trigger('submit');
+    equal($('.unhappy').length, 1);
+    equal($('.unhappyMessage').length, 1);
+
+    $('#textInput1').val('test');
+    form.trigger('submit');
+    equal($('.unhappy').length, 0);
+    equal($('.unhappyMessage').length, 0);
 });
 
 test("check setting required with 'required' attribute", function () {
-	var form = fixture('<input type="text" required id="textInput1" />');
-	
-	form.isHappy({
-	    fields: {
-	        '#textInput1': {
-	            message: 'Please enter an email'
-	        }
-	    },
-	    testMode: true
-	});
-	
-	form.trigger('submit');
-	equal($('.unhappy').length, 1);
-	equal($('.unhappyMessage').length, 1);
-	
-	$('#textInput1').val('test');
-	form.trigger('submit');
-	equal($('.unhappy').length, 0);
-	equal($('.unhappyMessage').length, 0);
+    var form = fixture('<input type="text" required id="textInput1" />');
+
+    form.isHappy({
+        fields: {
+            '#textInput1': {
+                message: 'Please enter an email'
+            }
+        },
+        testMode: true
+    });
+
+    form.trigger('submit');
+    equal($('.unhappy').length, 1);
+    equal($('.unhappyMessage').length, 1);
+
+    $('#textInput1').val('test');
+    form.trigger('submit');
+    equal($('.unhappy').length, 0);
+    equal($('.unhappyMessage').length, 0);
 });
 
 
 test("check email", function () {
-	var form = fixture('<input type="text" id="textInput1" />');
-	
-	form.isHappy({
-	    fields: {
-	        '#textInput1': {
-	            required: true,
-	            message: 'Please enter an email',
-	            test: happy.email
-	        }
-	    },
-	    testMode: true
-	});
-	
-	form.trigger('submit');
-	equal($('.unhappy').length, 1);
-	equal($('.unhappyMessage').length, 1);
-	
-	$('#textInput1').val('test');
-	form.trigger('submit');
-	equal($('.unhappy').length, 1);
-	equal($('.unhappyMessage').length, 1);
-	
-	$('#textInput1').val('hjoreteg@gmail.com');
-	form.trigger('submit');
-	equal($('.unhappy').length, 0);
-	equal($('.unhappyMessage').length, 0);
+    var form = fixture('<input type="text" id="textInput1" />');
+
+    form.isHappy({
+        fields: {
+            '#textInput1': {
+                required: true,
+                message: 'Please enter an email',
+                test: happy.email
+            }
+        },
+        testMode: true
+    });
+
+    form.trigger('submit');
+    equal($('.unhappy').length, 1);
+    equal($('.unhappyMessage').length, 1);
+
+    $('#textInput1').val('test');
+    form.trigger('submit');
+    equal($('.unhappy').length, 1);
+    equal($('.unhappyMessage').length, 1);
+
+    $('#textInput1').val('hjoreteg@gmail.com');
+    form.trigger('submit');
+    equal($('.unhappy').length, 0);
+    equal($('.unhappyMessage').length, 0);
 });
 
 
 test("test password match", function () {
     var form = fixture('<input type="password1" id="p1" /><input type="password2" id="p2" />');
-    
+
     form.isHappy({
         fields: {
             '#p1': {
@@ -114,21 +115,21 @@ test("test password match", function () {
         },
         testMode: true
     });
-    
+
     form.trigger('submit');
     equal($('.unhappy').length, 2);
     equal($('.unhappyMessage').length, 2);
-    
+
     $('#p1').val('test');
     form.trigger('submit');
     equal($('.unhappy').length, 2);
     equal($('.unhappyMessage').length, 2);
-    
+
     $('#p2').val('test2');
     form.trigger('submit');
     equal($('.unhappy').length, 1);
     equal($('.unhappyMessage').length, 1);
-    
+
     $('#p2').val('test');
     form.trigger('submit');
     equal($('.unhappy').length, 0);
@@ -136,53 +137,53 @@ test("test password match", function () {
 });
 
 test("test passed in 'clean' method", function () {
-	var form = fixture('<input type="text" id="textInput1" />');
-	
-	form.isHappy({
-	    fields: {
-	        '#textInput1': {
-	            message: 'Please enter an email',
-	            clean: function (val) {
-	                return val.replace("a", "b");
-	            }
-	        }
-	    },
-	    testMode: true
-	});
-	
-	
-	$('#textInput1').val('  a  ');
-	form.trigger('submit');
-	equal($('#textInput1').val(), '  b  ');
+    var form = fixture('<input type="text" id="textInput1" />');
+
+    form.isHappy({
+        fields: {
+            '#textInput1': {
+                message: 'Please enter an email',
+                clean: function (val) {
+                    return val.replace("a", "b");
+                }
+            }
+        },
+        testMode: true
+    });
+
+
+    $('#textInput1').val('  a  ');
+    form.trigger('submit');
+    equal($('#textInput1').val(), '  b  ');
 });
 
 test("test passing 'sometimes' to required", function () {
-	var form = fixture('<input type="text" id="textInput1" />'),
-	    testFlag = false;
-	
-	form.isHappy({
-	    fields: {
-	        '#textInput1': {
-	            message: 'Please enter an email',
-	            required: 'sometimes',
-	            test: function (val) {
-	                return testFlag;
-	            }
-	        }
-	    },
-	    testMode: true
-	});
-	
-	form.trigger('submit');
-	equal($('.unhappy').length, 1);
-	testFlag = true;
-	form.trigger('submit');
-	equal($('.unhappy').length, 0);
+    var form = fixture('<input type="text" id="textInput1" />'),
+    testFlag = false;
+
+    form.isHappy({
+        fields: {
+            '#textInput1': {
+                message: 'Please enter an email',
+                required: 'sometimes',
+                test: function (val) {
+                    return testFlag;
+                }
+            }
+        },
+        testMode: true
+    });
+
+    form.trigger('submit');
+    equal($('.unhappy').length, 1);
+    testFlag = true;
+    form.trigger('submit');
+    equal($('.unhappy').length, 0);
 });
 
 test("test required fields should only be tested on submit", function () {
     var form = fixture('<input type="text" id="textInput1" />');
-    
+
     form.isHappy({
         fields: {
             '#textInput1': {
@@ -192,7 +193,7 @@ test("test required fields should only be tested on submit", function () {
         },
         testMode: true
     });
-    
+
     $('#textInput1').trigger('blur');
     equal($('.unhappy').length, 0);
     form.trigger('submit');
@@ -201,7 +202,7 @@ test("test required fields should only be tested on submit", function () {
 
 test("test non-required fields still tested on blur", function () {
     var form = fixture('<input type="text" id="textInput1" />');
-    
+
     form.isHappy({
         fields: {
             '#textInput1': {
@@ -211,7 +212,7 @@ test("test non-required fields still tested on blur", function () {
         },
         testMode: true
     });
-    
+
     $('#textInput1').val('h@h').trigger('blur');
     equal($('.unhappy').length, 1);
     form.trigger('submit');
@@ -220,8 +221,8 @@ test("test non-required fields still tested on blur", function () {
 
 test("test unHappy callback", function () {
     var form = fixture('<input type="text" id="textInput1" required />'),
-        myFlag = false;
-    
+    myFlag = false;
+
     form.isHappy({
         fields: {
             '#textInput1': {
@@ -233,7 +234,7 @@ test("test unHappy callback", function () {
             myFlag = true;
         }
     });
-    
+
     form.trigger('submit');
     ok(myFlag);
     myFlag = false;
@@ -244,24 +245,24 @@ test("test unHappy callback", function () {
 
 test("test included email validator", function () {
     var happyEmails = [
-            'henrik@andyet.net',
-            'h.joreteg@gmail.com',
-            '23423.24.2.2342@test.test',
-            '2.3.2.4.a.a.a.ffasaf.a@aol.com'
-        ],
-        sadEmails = [
-            '.henrik@andyet.net',
-            'henrik.@andyet.net',
-            'test@test.afcom.com.',
-            'h@.h',
-            'a@a'           
-        ],
-        i;
-    
+        'henrik@andyet.net',
+        'h.joreteg@gmail.com',
+        '23423.24.2.2342@test.test',
+        '2.3.2.4.a.a.a.ffasaf.a@aol.com'
+    ],
+    sadEmails = [
+        '.henrik@andyet.net',
+        'henrik.@andyet.net',
+        'test@test.afcom.com.',
+        'h@.h',
+        'a@a'
+    ],
+    i;
+
     for (i = 0; i < happyEmails.length; i++) {
         ok(happy.email(happyEmails[i]));
     }
-    
+
     for (i = 0; i < sadEmails.length; i++) {
         ok(!happy.email(sadEmails[i]));
     }
@@ -269,21 +270,21 @@ test("test included email validator", function () {
 
 test("test included date validator", function () {
     var happyDates = [
-            '12/29/1982',
-            '11/02/2099'
-        ],
-        sadDates = [
-            '123/24/1999',
-            '13/31/2099',
-            '12/32/2099',
-            '1/31/2999'          
-        ],
-        i;
-    
+        '12/29/1982',
+        '11/02/2099'
+    ],
+    sadDates = [
+        '123/24/1999',
+        '13/31/2099',
+        '12/32/2099',
+        '1/31/2999'
+    ],
+    i;
+
     for (i = 0; i < happyDates.length; i++) {
         ok(happy.date(happyDates[i]));
     }
-    
+
     for (i = 0; i < sadDates.length; i++) {
         ok(!happy.date(sadDates[i]));
     }
@@ -291,22 +292,22 @@ test("test included date validator", function () {
 
 test("test included phone validator", function () {
     var happyPhones = [
-            '909-765-3941',
-            '(909) 234-2343',
-            '9999999999',
-            '(909)234-2343',
-        ],
-        sadPhones = [
-            '12-123-22311',
-            'asdfasdf',
-            '123-123-12344'
-        ],
-        i;
-    
+        '909-765-3941',
+        '(909) 234-2343',
+        '9999999999',
+        '(909)234-2343',
+    ],
+    sadPhones = [
+        '12-123-22311',
+        'asdfasdf',
+        '123-123-12344'
+    ],
+    i;
+
     for (i = 0; i < happyPhones.length; i++) {
         ok(happy.USPhone(happyPhones[i]));
     }
-    
+
     for (i = 0; i < sadPhones.length; i++) {
         ok(!happy.USPhone(sadPhones[i]));
     }
@@ -317,18 +318,18 @@ test("check return value", function () {
     equal(form.isHappy({fields: {}}), form);
 });
 
-test("test message is empty string when not explicitly set", function() {
-  var form = fixture('<input type="text" id="textInput1" />');
+test("test message is empty string when not explicitly set", function () {
+    var form = fixture('<input type="text" id="textInput1" />');
 
-  form.isHappy({
-      fields: {
-          '#textInput1': {
-              required: true,
-          }
-      },
-      testMode: true
-  });
+    form.isHappy({
+        fields: {
+            '#textInput1': {
+                required: true,
+            }
+        },
+        testMode: true
+    });
 
-  form.trigger('submit');
-  equal($('.unhappyMessage').first().text(), '');
+    form.trigger('submit');
+    equal($('.unhappyMessage').first().text(), '');
 });
